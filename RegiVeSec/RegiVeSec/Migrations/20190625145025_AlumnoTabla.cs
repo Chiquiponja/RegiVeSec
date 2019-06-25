11138a7f01500120ffad962254a54741c0a77831
priscila guzman <priscilaelisaguzman34@gmail.com>
2019-06-25 12:09:19 -0300

    Migracion Vehiculo

---
+++ RegiVeSec/RegiVeSec/Migrations/20190625145025_AlumnoTabla.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegiVeSec.Migrations
+{
+    public partial class AlumnoTabla : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
