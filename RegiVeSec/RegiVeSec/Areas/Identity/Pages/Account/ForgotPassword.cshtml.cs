3be9c1796b2b0829784d2fff671531203f844a42
priscila guzman <priscilaelisaguzman34@gmail.com>
2019-07-22 11:56:45 -0300

    Recuperacion de contraseña

--- RegiVeSec/RegiVeSec/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ RegiVeSec/RegiVeSec/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -41,24 +42,42 @@ namespace RegiVeSec.Areas.Identity.Pages.Account
                 if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
+                    var To = Input.Email;
+                    var From = "priscilaelisaguzman34@gmail.com";
+                    //Ingrese su email
+                    var Password = "valunchosehun19994"; //Ingrese Contraseña
+                    var Puerto = 587;
+                    var SMTP = "smtp.gmail.com";
+                    var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ResetPassword",
+                        pageHandler: null,
+                        values: new { code },
+                        protocol: Request.Scheme);
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(From);// email del formulario
+
+                    msz.To.Add(To); //correo que recive el mensaje
+                    msz.Subject = "Restablecer Contraseña";// asunto del mensaje
+                    msz.IsBodyHtml = true;// habilito formato html para que quede flama el correo
+                    msz.Body = $@"
+                               <strong>Restablecer Contraseña: </strong>{$"Por favor restablece tu contraseña <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>haciendo clic aquí</a>."}<br>
+                               ";
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = SMTP; //smtp varia depende del correo
+                    smtp.Port = Puerto; // puerto varia depende el correo
+                    smtp.Credentials = new System.Net.NetworkCredential(From, Password);// correo que va a enviar el mensaje y su contraseña gmail
+                    smtp.EnableSsl = true;
+                    smtp.Send(msz);
                     return RedirectToPage("./ForgotPasswordConfirmation");
                 }
 
                 // For more information on how to enable account confirmation and password reset please 
                 // visit https://go.microsoft.com/fwlink/?LinkID=532713
-                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
-                var callbackUrl = Url.Page(
-                    "/Account/ResetPassword",
-                    pageHandler: null,
-                    values: new { code },
-                    protocol: Request.Scheme);
+                
 
-                await _emailSender.SendEmailAsync(
-                    Input.Email,
-                    "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                return RedirectToPage("./ForgotPasswordConfirmation");
+                
+                
             }
 
             return Page();
