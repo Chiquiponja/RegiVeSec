b4015938c1d489bc3c063ef7fc79a8e0a456c5d4
priscila guzman <priscilaelisaguzman34@gmail.com>
2019-07-26 15:34:05 -0300

    Trabajo desde cordoba

--- RegiVeSec/RegiVeSec/Models/VehiculoRegiVeSec.cs
+++ RegiVeSec/RegiVeSec/Models/VehiculoRegiVeSec.cs
@@ -12,59 +12,59 @@ namespace RegiVeSec.Models
         public DateTime FechaDeIngreso { get; set; }
 
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Propietario { get; set; }
         
             [MaxLength(30)]
-     [Required]
+            [Required]
             public string Dominio  { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Tipo { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Marca { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Color  { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Modelo { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Causa { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Estado { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string NumeroSumario { get; set; }
         
         [MaxLength(30)]
- [Required]
+         [Required]
         public string Dependencia  { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Orden  { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string DependenciaProcedente { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Observaciones { get; set; }
         
         [MaxLength(30)]
- [Required]
+        [Required]
         public string Recibe  { get; set; }
         
         [MaxLength(30)]
