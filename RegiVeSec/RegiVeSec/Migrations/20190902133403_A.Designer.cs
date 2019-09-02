45bb8bc891e1edee6aa42b4344134f16a0eb4a2e
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-09-02 11:38:05 -0300

    modificacion de vista de detalles

--- RegiVeSec/RegiVeSec/Migrations/20190902133403_A.Designer.cs
+++ RegiVeSec/RegiVeSec/Migrations/20190902133403_A.Designer.cs
@@ -10,14 +10,14 @@ using RegiVeSec.Data;
 namespace RegiVeSec.Migrations
 {
     [DbContext(typeof(Conexionbd))]
-    [Migration("20190902121409_A")]
+    [Migration("20190902133403_A")]
     partial class A
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.3-servicing-35854")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
